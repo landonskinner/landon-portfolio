08bf736c187f97a7e1c160248b4164a87cc0ec3c
Landon Skinner <landonjskinner@gmail.com>
2024-04-28 14:02:34 -0600

    hero section - styling and animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,45 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-pattern": 'url("/images/background.svg")',
+      },
+      animation: {
+        "enter-left": "enterLeft 1s ease forwards",
+        "enter-right": "enterRight 1s ease forwards",
+      },
+      keyframes: {
+        enterLeft: {
+          "0%": {
+            transform: "translateX(-200%)",
+          },
+          "60%": {
+            transform: "translateX(20%) skew(-10deg)",
+          },
+          "80%": {
+            transform: "translateX(-15%) skew(5deg)",
+          },
+          "100%": {
+            transform: "translateX(-5%) skew(0deg)",
+          },
+        },
+        enterRight: {
+          "0%": {
+            transform: "translateX(200%)",
+          },
+          "60%": {
+            transform: "translateX(-20%) skew(10deg)",
+          },
+          "80%": {
+            transform: "translateX(15%) skew(-5deg)",
+          },
+          "100%": {
+            transform: "translateX(5%) skew(0deg)",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
